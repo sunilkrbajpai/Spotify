07428fab13b4c212bc30a9045f4428ff2c271cf9
sunil <sunilkumarbajpai.skb@gmail.com>
2020-08-11 16:51:14 +0530

    Updates

---
+++ precache-manifest.945dfff4006beec6e4394baf3848fbe0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec51e076b0670d2e147ab8f5749f84d",
+    "url": "/Spotify/index.html"
+  },
+  {
+    "revision": "5805b0477b8c78edfe40",
+    "url": "/Spotify/static/css/main.49e180db.chunk.css"
+  },
+  {
+    "revision": "8c641921a9f4aadfbe06",
+    "url": "/Spotify/static/js/2.99329927.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/Spotify/static/js/2.99329927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5805b0477b8c78edfe40",
+    "url": "/Spotify/static/js/main.3c6b6510.chunk.js"
+  },
+  {
+    "revision": "2ca866082d361f3d6767",
+    "url": "/Spotify/static/js/runtime-main.a6619ebf.js"
+  },
+  {
+    "revision": "da234c8ad70a86a3cfe624699deac1c9",
+    "url": "/Spotify/static/media/logo.da234c8a.gif"
+  },
+  {
+    "revision": "79e157ad0ba50806ad877bab10fd0b99",
+    "url": "/Spotify/static/media/spotify.79e157ad.jpg"
+  }
+]);
